53eca09c507b68d6b2d1e9f699799884a3fed111
m87 <bartosz.radlinski@gmail.com>
2025-12-28 17:00:11 +0100

    package

--- query.go
+++ query.go
@@ -466,6 +466,12 @@ SELECT * FROM path;
 		}
 	}
 	
+	fmt.Println("Debug: Building ancestor tree for childID =", childID)
+	fmt.Println("Debug: Retrieved nodes:")
+	for _, n := range nodes {
+		fmt.Printf("  Node ID: %s, Parent ID: %v\n", n.Core.Id, n.Core.ParentId)
+	}
+
 	byID := make(map[string]*TreeNode, len(nodes))
 	for _, n := range nodes {
 		byID[n.Core.Id] = &TreeNode{
