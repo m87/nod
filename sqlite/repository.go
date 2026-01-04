f59f772ba17c7e0c7535be973522b081d2fa975e
m87 <bartosz.radlinski@gmail.com>
2026-01-04 12:33:34 +0100

    generics

--- sqlite/repository.go
+++ sqlite/repository.go
@@ -8,19 +8,15 @@ import (
 	"gorm.io/gorm"
 )
 
-func NewRepository[T nod.NodeModel](path string, log *slog.Logger) *nod.Repository[T] {
-	db := InitDB(log, path)
-	return NewRepiositoryWithDB[T](db, log)
-}
-
-func NewRepiositoryWithDB[T nod.NodeModel](db *gorm.DB, log *slog.Logger) *nod.Repository[T] {
+func NewRepository[T nod.NodeModel](path string, log *slog.Logger, mappers *nod.MapperRegistry[T]) *nod.Repository[T] {
 	return &nod.Repository[T]{
-		Db:   db,
+		Db:   initDB(log, path),
 		Log:  log,
+		Mappers: mappers,
 	}
 }
 
-func InitDB(log *slog.Logger, path string) *gorm.DB {
+func initDB(log *slog.Logger, path string) *gorm.DB {
 	log.Debug(">> open database", slog.String("path", path))
 	db, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
 	if err != nil {
