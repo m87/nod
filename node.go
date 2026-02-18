b257614f29f744ccfab61710ae87da69cb9994f0
m87 <bartosz.radlinski@gmail.com>
2026-02-18 23:03:37 +0100

    wip

--- node.go
+++ node.go
@@ -8,9 +8,9 @@ type NodeCore struct {
 	Id          string    `gorm:"type:char(36);primaryKey"`
 	NamespaceId *string   `gorm:"type:char(36);index:idx_namespace_id,priority:1;index"`
 	ParentId    *string   `gorm:"type:char(36);index:idx_parent_id,priority:2;index"`
-	Type				string    `gorm:"type:text;not null;index;default:''"`
+	Kind				string    `gorm:"type:text;not null;index;default:''"`
 	Status      string    `gorm:"type:text;not null;index;default:''"`
-	Kind        string    `gorm:"type:text;not null;index"`
+	Name        string    `gorm:"type:text;not null;index"`
 	CreatedAt   time.Time `gorm:"type:datetime;not null;autoCreateTime"`
 	UpdatedAt   time.Time `gorm:"type:datetime;not null;autoUpdateTime"`
 }
